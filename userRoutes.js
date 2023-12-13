88b974ed0db81da01926ebfda34bb336e0384427
Aviran Itzhak <98554893+aviranDev@users.noreply.github.com>
2023-12-14 01:12:22 +0200

    server

--- userRoutes.js
+++ userRoutes.js
@@ -4,7 +4,7 @@ const User = require('./model');
 const router = express.Router();
 
 // Example route to create a new user
-router.post('/create-user', async (req, res) => {
+router.get('/create-user', async (req, res) => {
   try {
     const newUser = new User({ name: 'John Doe', email: 'john@example.com' });
     await newUser.save();
