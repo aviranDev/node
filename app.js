1d578919b31f6514af7ab49c1b0598e820b31d7b
Aviran Itzhak <98554893+aviranDev@users.noreply.github.com>
2023-12-13 02:54:17 +0200

    variable port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Weolcome to the home page!');
 });
 
-const port = 3000
+const port = process.env.port || 3000
 app.listen(port, () => {
   console.log("Server runs on port:", port);
 });
