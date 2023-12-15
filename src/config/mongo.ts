a872df2b463d940c76feb7941d2dc5e091c0b172
Aviran Itzhak <98554893+aviranDev@users.noreply.github.com>
2023-12-16 01:30:52 +0200

    server

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -2,9 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const username = process.env.ATALS_USERNAME || '';
-const password = process.env.ATALS_PASSWORD || '';
-const atals_connection = process.env.ATALS_CONNECTION || '';
-const atals_db = process.env.ATALS_DB || '';
+const username = process.env.ATALS_USERNAME;
+const password = process.env.ATALS_PASSWORD;
+const atals_connection = process.env.ATALS_CONNECTION;
+const atals_db = process.env.ATALS_DB;
 
 export const atlas_uri = `mongodb+srv://${username}:${password}@${atals_connection}/${atals_db}`;
