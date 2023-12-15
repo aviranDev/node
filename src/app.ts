a39b6ad8dd1ca053752bcd5357aed59068e6b58e
Aviran Itzhak <98554893+aviranDev@users.noreply.github.com>
2023-12-15 20:11:40 +0200

    server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from './userRoutes';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.port || 3000;
 
 if (process.argv[2] === '--production') {
-  mongoose.connect('mongodb+srv://aviran:304715840@cluster0.g4o9h.mongodb.net/testDb1?retryWrites=true&w=majority')
+  mongoose.connect(process.env.mongoDbAtals)
     .then(() => {
       console.log('Connected to MongoDB atals successfully!');
     })
@@ -14,7 +16,7 @@ if (process.argv[2] === '--production') {
       console.error('Error connecting to MongoDB atals:', err);
     });
 } else if (process.argv[2] === '--development') {
-  mongoose.connect('mongodb://127.0.0.1/testdb')
+  mongoose.connect(process.env.mongoDbCompass)
     .then(() => {
       console.log('Connected to MongoDB compass successfully!');
     })
