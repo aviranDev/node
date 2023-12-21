ed5683e1bf02769ccafab38c8bcbe3945f780b7a
Aviran Itzhak <98554893+aviranDev@users.noreply.github.com>
2023-12-21 02:19:00 +0200

    server

---
+++ src/errors/errorHandler.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+
+/**
+ * Error handling middleware responsible for logging errors and sending appropriate responses.
+ *
+ * This middleware logs detailed error information and responds to clients with the appropriate error status code and message.
+ * @param {Error} err - The error object.
+ * @param {Request} req - The Express request object.
+ * @param {Response} res - The Express response object.
+ * @param {NextFunction} next - The Express next function.
+ */
+const errorHandler = function (err: Error, req: Request, res: Response, next: NextFunction): void {
+  try {
+    // Handle Resource Not Found (404) Error
+    console.log(err.message);
+
+    // Respond with the appropriate status code, message, and error details
+    res.status(500).json({
+      message: err.message,
+    });
+
+
+  } catch (error) {
+    // Handle any errors that might occur during the response generation
+    res.status(500).json({
+      error: {
+        status: 500,
+        name: "Internal Server Error",
+        message: "An unexpected error occurred while processing the request.",
+      },
+    });
+  }
+}
+
+// Export the middlewares functions
+export { errorHandler };
